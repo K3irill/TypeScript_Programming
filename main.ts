6b7af84b474cfbb7c2dd7399c0632d8ab9410c7e
K3rill <razdor.2019@mail.ru>
2024-09-21 21:52:00 +0300

    types

---
+++ main.ts
@@ -0,0 +1,25 @@
+let entityOne: number = 1;
+//
+let entityTwo: string = "something";
+//
+let entityThree: boolean = true;
+//
+let entityFour: undefined;
+//
+let entityFive: null;
+//
+let entitySix: any;
+//
+let entitySeven: unknown;
+//
+let entityEight: void; //empty-пустота
+//
+
+enum links {
+  youtube = "https://youtube.com/",
+  vk = "https://vk.com",
+  facebook = "https://facebook.com/",
+}
+
+links.youtube;
+links.vk;
