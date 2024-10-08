79c6a329eeb301976b03de835761f74399b96bf6
Kirill <razdor.2019@mail.ru>
2024-10-08 21:29:18 +0300

    interface

---
+++ main.js
@@ -0,0 +1,62 @@
+var entityOne = 1;
+//
+var entityTwo = "something";
+//
+var entityThree = true;
+//
+var entityFour;
+//
+var entityFive;
+//
+var entitySix;
+//
+var entitySeven;
+//
+var entityEight; //empty-пустота
+//
+var links;
+(function (links) {
+    links["youtube"] = "https://youtube.com/";
+    links["vk"] = "https://vk.com";
+    links["facebook"] = "https://facebook.com/";
+})(links || (links = {}));
+links.youtube;
+links.vk;
+var bookType = 'Big Ocean';
+var books = ['Tom Sawyer', 'Alice', 'Home alone'];
+var users = {
+    name: 'Kail',
+    age: 20
+};
+var receipt = {
+    price: 120,
+    date: '15.10.2024',
+    title: 'Order №1',
+    subtitle: 'Kail\'s Cafe',
+    position1: {
+        price: 185,
+        title: 'mango - bubble tea',
+        isReady: false
+    },
+    position2: {
+        price: 215,
+        title: 'coffe & one bagel',
+        isReady: true
+    }
+};
+var receiptIn = {
+    price: 120,
+    date: '15.10.2024',
+    title: 'Order №1',
+    subtitle: 'Kail\'s Cafe',
+    position1: {
+        price: 185,
+        title: 'mango - bubble tea',
+        isReady: false
+    },
+    position2: {
+        price: 215,
+        title: 'coffe & one bagel',
+        isReady: true
+    }
+};
--- main.ts
+++ main.ts
@@ -23,3 +23,88 @@ enum links {
 
 links.youtube;
 links.vk;
+
+let bookType : string | null = 'Big Ocean' 
+const books : string[] = ['Tom Sawyer', 'Alice',  'Home alone']
+
+let users : { name: string; age: number} = {
+  name: 'Kail',
+  age: 20
+}
+
+
+// type 
+// нельзя переопределять свойства после создания.
+type ReceiptType = {
+  price: number,
+  date: string,
+  title: string,
+  subtitle: string,
+  position1: {
+    price: number,
+    title: string,
+    isReady: boolean,
+  }
+  position2: {
+    price: number,
+    title: string,
+    isReady: boolean,
+  }
+}
+
+let receipt: ReceiptType = {
+  price: 120,
+  date: '15.10.2024',
+  title: 'Order №1',
+  subtitle: 'Kail\'s Cafe',
+  position1: {
+    price: 185,
+    title: 'mango - bubble tea',
+    isReady: false
+
+  },
+  position2: {
+    price: 215,
+    title: 'coffe & one bagel',
+    isReady: true
+  }
+  
+}
+//or 
+
+//interface
+interface ReceiptTypeIn  {
+  price: number,
+  date: string,
+  title: string,
+  subtitle: string,
+  position1: {
+    price: number,
+    title: string,
+    isReady: boolean,
+  }
+  position2: {
+    price: number,
+    title: string,
+    isReady: boolean,
+  }
+}
+
+let receiptIn: ReceiptTypeIn = {
+  price: 120,
+  date: '15.10.2024',
+  title: 'Order №1',
+  subtitle: 'Kail\'s Cafe',
+  position1: {
+    price: 185,
+    title: 'mango - bubble tea',
+    isReady: false
+
+  },
+  position2: {
+    price: 215,
+    title: 'coffe & one bagel',
+    isReady: true
+  }
+  
+}
