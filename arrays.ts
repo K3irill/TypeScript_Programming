8786f94ef21f593f1bba74a264cf1832beeb49dc
Kirill <razdor.2019@mail.ru>
2024-10-09 13:51:02 +0300

    arrays and readonly

---
+++ arrays.ts
@@ -0,0 +1,30 @@
+const smartphones: string[] = [
+  "samsung",
+  "huawei",
+  "xiaomi",
+  "iphone",
+  "google",
+  "nothing phone",
+];
+//or
+const smartphones2: Array<string> = [
+  "samsung",
+  "huawei",
+  "xiaomi",
+  "iphone",
+  "google",
+  "nothing phone",
+];
+//or
+const smartphones3 = new Array<string>(
+  "samsung",
+  "huawei",
+  "xiaomi",
+  "iphone",
+  "google",
+  "nothing phone"
+);
+
+//redonly
+
+let list: readonly number[] = [2024, 1998, 2011];
--- main.ts
+++ main.ts
@@ -24,87 +24,94 @@ enum links {
 links.youtube;
 links.vk;
 
-let bookType : string | null = 'Big Ocean' 
-const books : string[] = ['Tom Sawyer', 'Alice',  'Home alone']
+let bookType: string | null = "Big Ocean";
+const books: string[] = ["Tom Sawyer", "Alice", "Home alone"];
 
-let users : { name: string; age: number} = {
-  name: 'Kail',
-  age: 20
-}
+let users: { name: string; age: number } = {
+  name: "Kail",
+  age: 20,
+};
 
-
-// type 
+// type
 // нельзя переопределять свойства после создания.
 type ReceiptType = {
-  price: number,
-  date: string,
-  title: string,
-  subtitle: string,
+  price: number;
+  date: string;
+  title: string;
+  subtitle: string;
   position1: {
-    price: number,
-    title: string,
-    isReady: boolean,
-  }
+    price: number;
+    title: string;
+    isReady: boolean;
+  };
   position2: {
-    price: number,
-    title: string,
-    isReady: boolean,
-  }
-}
+    price: number;
+    title: string;
+    isReady: boolean;
+  };
+};
 
 let receipt: ReceiptType = {
   price: 120,
-  date: '15.10.2024',
-  title: 'Order №1',
-  subtitle: 'Kail\'s Cafe',
+  date: "15.10.2024",
+  title: "Order №1",
+  subtitle: "Kail's Cafe",
   position1: {
     price: 185,
-    title: 'mango - bubble tea',
-    isReady: false
-
+    title: "mango - bubble tea",
+    isReady: false,
   },
   position2: {
     price: 215,
-    title: 'coffe & one bagel',
-    isReady: true
-  }
-  
-}
-//or 
+    title: "coffe & one bagel",
+    isReady: true,
+  },
+};
+//or
 
 //interface
-interface ReceiptTypeIn  {
-  price: number,
-  date: string,
-  title: string,
-  subtitle: string,
+interface ReceiptTypeIn {
+  price: number;
+  date: string;
+  title: string;
+  subtitle: string;
   position1: {
-    price: number,
-    title: string,
-    isReady: boolean,
-  }
+    price: number;
+    title: string;
+    isReady: boolean;
+  };
   position2: {
-    price: number,
-    title: string,
-    isReady: boolean,
-  }
+    price: number;
+    title: string;
+    isReady: boolean;
+  };
 }
 
 let receiptIn: ReceiptTypeIn = {
   price: 120,
-  date: '15.10.2024',
-  title: 'Order №1',
-  subtitle: 'Kail\'s Cafe',
+  date: "15.10.2024",
+  title: "Order №1",
+  subtitle: "Kail's Cafe",
   position1: {
     price: 185,
-    title: 'mango - bubble tea',
-    isReady: false
-
+    title: "mango - bubble tea",
+    isReady: false,
   },
   position2: {
     price: 215,
-    title: 'coffe & one bagel',
-    isReady: true
-  }
-  
-}
+    title: "coffe & one bagel",
+    isReady: true,
+  },
+};
+
+let book: any = 0;
+
+book = "Game of Thrones";
+book = false;
+book();
+book.what.a.hell = "LOL";
+
+let testUnknown: unknown = 0;
+
+testUnknown = "Game of Thrones";
+testUnknown = true;
