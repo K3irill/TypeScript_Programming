0e3d05df29c0c8c063bb637c7b91f0c6573f3985
Kirill <razdor.2019@mail.ru>
2024-10-09 22:18:36 +0300

    generics

--- generics.ts
+++ generics.ts
@@ -1,12 +1,36 @@
-class Hotel {
-	private freeRoomsCount: number
-	constructor(freeRoomsCount: number) {
-		this.freeRoomsCount = freeRoomsCount
-	}
-	public takeARoom(): void {
-		this.freeRoomsCount--
-	}
-	public isRoomAvailable(): boolean {
-		return this.freeRoomsCount > 0
+class Human {
+	public name: string = 'Ivan'
+}
+class Carr {
+	public number: number = 123
+}
+class Dog {
+	public hasFourLegs: boolean = true
+}
+
+class Robot {
+	public bipbop: boolean = true
+}
+class Camera<T> {
+	private objects: Array<T> = []
+	public collectObject(object: T): void {
+		this.objects.push(object)
 	}
 }
+
+const human = new Human()
+const carr = new Car()
+const dog = new Dog()
+
+const camera1 = new Camera<Human>()
+const camera2 = new Camera<Carr>()
+
+//
+
+class TypeChecker<T> {
+	public getType(value: T): string {
+		return typeof value
+	}
+}
+const checker = new TypeChecker<number | string | undefined>()
+console.log(checker.getType(1))
