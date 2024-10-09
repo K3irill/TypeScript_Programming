e281d01e3f895e57ab366d3a5a3907352ecc8d38
Kirill <razdor.2019@mail.ru>
2024-10-09 15:02:19 +0300

    function

---
+++ function.ts
@@ -0,0 +1,28 @@
+// examples
+function foo(param1: string, param2: number): number {
+  return param1.length + param2;
+}
+
+const fooo = (param1: boolean, param2: number[]): number[] => {
+  return param1 ? param2.slice(0, 1) : param2.slice(1, 2);
+};
+
+//--------------------
+
+function reverse(str: string): string {
+  if (str === "") return str;
+  const lastIdx = str.length - 1;
+  return str[lastIdx] + reverse(str.slice(0, lastIdx));
+}
+console.log(reverse("hello"));
+
+// void
+function log(): void {
+  console.log("hi everyone!");
+}
+
+//never
+
+function error(message: string): never {
+  throw new Error(message);
+}
